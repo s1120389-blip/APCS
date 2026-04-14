7ab3ad6265a7ef6893da6925164dd64b191d9cf2
s1120389-blip <s1120389@dmhs.kh.edu.tw>
2026-04-14 11:28:56 +0800

    Create GCD.cpp

---
+++ GCD.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int x, y;
+	cin >> x >> y;
+	
+	while (x % y != 0){
+		int temp = x %y;
+		x = y;
+		y = temp;
+	}
+	cout << y << "\n";
+	return 0;
+} 
